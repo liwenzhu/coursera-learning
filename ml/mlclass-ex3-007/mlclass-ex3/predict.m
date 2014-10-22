1afaafc99fe373a3ce493f5fdc10fe44c742feea
Vince <lee.wenzhu@gmail.com>
2014-10-22 09:06:20 +0000

    implementation of neural network predict.

--- ml/mlclass-ex3-007/mlclass-ex3/predict.m
+++ ml/mlclass-ex3-007/mlclass-ex3/predict.m
@@ -7,6 +7,8 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
+X = [ones(m,1) X];
+
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
@@ -21,9 +23,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = sigmoid(X * Theta1');
+m = size(X, 1);
 
+X = [ones(m, 1) X];
+X = sigmoid(X * Theta2');
 
-
+[tmp, p] = max(X, [], 2);
 
 
 
