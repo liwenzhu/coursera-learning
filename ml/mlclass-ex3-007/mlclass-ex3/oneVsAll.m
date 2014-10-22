f0ff8a891b6df461ea36fb6e323ab478698c24a6
Vince <lee.wenzhu@gmail.com>
2014-10-22 08:34:18 +0000

    finish the implementation of predictOneVsAll.

--- ml/mlclass-ex3-007/mlclass-ex3/oneVsAll.m
+++ ml/mlclass-ex3-007/mlclass-ex3/oneVsAll.m
@@ -49,7 +49,16 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+K = num_labels;
+initial_theta = zeros(n+1, 1);
 
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+
+for i = 1:K,
+    [theta] = fmincg(@(t)(lrCostFunction(t, X, (y == i), lambda)), ...
+		initial_theta, options);
+    all_theta(i, :) = theta';
+end;
 
 
 
